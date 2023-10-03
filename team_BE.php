542a6467692d8c3d8111a7a34bf3029909e2d257
Nanfack Steve <nanfacksteve7@gmail.com>
2023-10-03 15:03:19 +0100

    update index and BE (delete 1 person)

--- team_BE.php
+++ team_BE.php
@@ -2,8 +2,7 @@
     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
         <div class="team-item">
             <div class="overflow-hidden position-relative">
-                <img class="img-fluid" src="img/organism/Directeur_Executif.jpg" alt=""
-                    style="height: 280px; width: 100%;">
+                <img class="img-fluid" src="img/organism/Directeur_Executif.jpg" alt="" style="height: 280px; width: 100%;">
                 <div class="team-social">
                     <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
@@ -67,8 +66,7 @@
     <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
         <div class="team-item">
             <div class="overflow-hidden position-relative">
-                <img class="img-fluid" src="img/organism/Tresorier_Gen_Adjoint.jpg" alt=""
-                    style="height: 280px; width: 100%;">
+                <img class="img-fluid" src="img/organism/Tresorier_Gen_Adjoint.jpg" alt="" style="height: 280px; width: 100%;">
                 <div class="team-social">
                     <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
                     <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
@@ -81,21 +79,4 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
-        <div class="team-item">
-            <div class="overflow-hidden position-relative">
-                <img class="img-fluid" src="img/avatar-anonyme.png" alt="" style="height: 280px; width: 100%;">
-                <div class="team-social">
-                    <a class="btn btn-square" href=""><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-square" href=""><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-square" href=""><i class="fab fa-instagram"></i></a>
-                </div>
-            </div>
-            <div class="text-center border border-5 border-light border-top-0 p-4">
-                <h5 class="mb-0">Full Name</h5>
-                <small>Designation</small>
-            </div>
-        </div>
-    </div>
-
 </div>
