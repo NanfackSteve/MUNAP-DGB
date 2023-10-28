23df6b432cf5db9ff7726a14945b823fc35350ce
Nanfack Steve <nanfacksteve7@gmail.com>
2023-10-28 14:41:35 +0100

    update Stats (flash region)

--- rules_summary.php
+++ rules_summary.php
@@ -3,7 +3,7 @@
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne">
             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                <a href="#chap-1">Chapitre I : DES DISPOSITIONS GÉNÉRALES</a>
+                Chapitre I : DES DISPOSITIONS GÉNÉRALES
             </button>
         </h2>
         <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#sommaire">
