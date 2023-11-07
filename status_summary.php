bd674a469ab0dc43d011c78ffbcbf062f9d456b6
Nanfack Steve <nanfacksteve7@gmail.com>
2023-11-07 17:54:02 +0100

    completed translate status pages

--- status_summary.php
+++ status_summary.php
@@ -1,70 +1,79 @@
-<h4 class="m-2 text-center">Sommaire :</h4>
+<h4 class="m-2 text-center"><?= $lang['status']['summary'] ?></h4>
 <div class="accordion " id="sommaire">
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                TITRE I : DES DISPOSITIONS GENERALES
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <?= $lang['status']['sum-title-1'] ?>
             </button>
         </h2>
-        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#sommaire">
+        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#title1-chap1">Chapitre I : DE LA CREATION DU SIEGE </a><br>
-                <a href="#title1-chap2">Chapitre II : DE L'OBJET</a>
+                <a href="#title1-chap1"><?= $lang['status']['title-1-chap-1'] ?></a><br>
+                <a href="#title1-chap2"><?= $lang['status']['title-1-chap-2'] ?></a>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                TITRE II : DE L'ORGANISATION ET DU FONCTIONNEMENT
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <?= $lang['status']['sum-title-2'] ?>
             </button>
         </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#sommaire">
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#title2-chap1">Chapitre I : DE LA QUALITE DE MEMBRE</a><br>
-                <a href="#title2-chap2">Chapitre II : DE L'ADMINISTRATION</a><br>
+                <a href="#title2-chap1"><?= $lang['status']['title-2-chap-1'] ?></a><br>
+                <a href="#title2-chap2"><?= $lang['status']['title-2-chap-2'] ?></a><br>
                 <span>
-                    <a class="nested-paragraph" href="#title2-chap2-sect1">Section I: De l'Assemblée Général</a><br>
-                    <a class="nested-paragraph" href="#title2-chap2-sect2">Section II: Du Conseil
-                        d'Administration</a><br>
-                    <a class="nested-paragraph" href="#title2-chap2-sect3">Section III: Du Bureau Exécutif</a><br>
-                    <a class="nested-paragraph" href="#title2-chap2-sect4">Section IV: De la Commission
-                        Financière</a><br>
+                    <a class="nested-paragraph"
+                        href="#title2-chap2-sect1"><?= $lang['status']['title-2-chap-2-sect-1'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title2-chap2-sect2"><?= $lang['status']['title-2-chap-2-sect-2'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title2-chap2-sect3"><?= $lang['status']['title-2-chap-2-sect-3'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title2-chap2-sect4"><?= $lang['status']['title-2-chap-2-sect-4'] ?></a><br>
                 </span>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                TITRE III : DES DISPOSITIONS FINANCIERES
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <?= $lang['status']['sum-title-3'] ?>
             </button>
         </h2>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#sommaire">
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#title3-chap1">Chapitre I : DES RESSOURCES</a><br>
-                <a href="#title3-chap2">Chapitre II : DES EMPLOIS</a><br>
+                <a href="#title3-chap1"><?= $lang['status']['title-3-chap-1'] ?></a><br>
+                <a href="#title3-chap2"><?= $lang['status']['title-3-chap-2'] ?></a><br>
                 <span>
-                    <a class="nested-paragraph" href="#title3-chap2-sect1">Section I: Allocation de Décès</a><br>
-                    <a class="nested-paragraph" href="#title3-chap2-sect2">Section II: Remboursement des frais
-                        d'Hôspitalisation, Médicaux
-                        et/ou Pharmaceutiques </a><br>
-                    <a class="nested-paragraph" href="#title3-chap2-sect3">Section III: Allocation de Mariage et
-                        Accouchement</a><br>
-                    <a class="nested-paragraph" href="#title3-chap2-sect4">Section IV: Allocation de décoration et
-                        départ à la
-                        retraite</a><br>
+                    <a class="nested-paragraph"
+                        href="#title3-chap2-sect1"><?= $lang['status']['title-3-chap-2-sect-1'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title3-chap2-sect2"><?= $lang['status']['title-3-chap-2-sect-2'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title3-chap2-sect3"><?= $lang['status']['title-3-chap-2-sect-3'] ?></a><br>
+                    <a class="nested-paragraph"
+                        href="#title3-chap2-sect4"><?= $lang['status']['title-3-chap-2-sect-4'] ?></a><br>
                 </span>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingFour">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                TITRE IV : DES DISPOSITIONS DIVERSES, TRANSITOIRES ET FINALES
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <?= $lang['status']['sum-title-4'] ?>
             </button>
         </h2>
-        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#sommaire">
+        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
                 ...
             </div>
