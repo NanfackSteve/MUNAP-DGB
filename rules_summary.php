c02ca733fe0d3f33789462e8bd482652f57a04b6
Nanfack Steve <nanfacksteve7@gmail.com>
2023-11-07 18:36:47 +0100

    completed translate rules pages

--- rules_summary.php
+++ rules_summary.php
@@ -1,83 +1,99 @@
-<h4 class="m-2 text-center">Sommaire :</h4>
+<h4 class="m-2 text-center"><?= $lang['status']['summary'] ?></h4>
 <div class="accordion " id="sommaire">
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingOne">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
-                Chapitre I : DES DISPOSITIONS GÉNÉRALES
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                <?= $lang['rules']['sum-chap-1'] ?>
             </button>
         </h2>
-        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#sommaire">
+        <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap-1">Chapitre 1 : Des Dispositions Générales</a>
+                <a href="#chap-1"><?= $lang['rules']['sum-chap-1'] ?></a>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingTwo">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                Chapitre II : DU FONCTIONNEMENT DES ORGANES D’ADMINISTRATION DE LA MUTUELLE
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <?= $lang['rules']['sum-chap-2'] ?>
             </button>
         </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#sommaire">
+        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap2-sect1">Section I : De l'Assemblée Générale</a><br>
-                <a href="#chap2-sect2">Section II : Du Conseil d'Administration</a><br>
-                <a href="#chap2-sect3">Section III : Du Bureau Exécutif</a><br>
+                <a href="#chap2-sect1"><?= $lang['rules']['chap-2-sect-1'] ?></a><br>
+                <a href="#chap2-sect2"><?= $lang['rules']['chap-2-sect-2'] ?></a><br>
+                <a href="#chap2-sect3"><?= $lang['rules']['chap-2-sect-3'] ?></a><br>
                 <span>
-                    <a class="nested-paragraph" href="#chap2-sect3-para1">Para I: Du Directeur Executif</a><br>
-                    <a class="nested-paragraph" href="#chap2-sect3-para2">Para II: Du Secrétaire Général/Adjoint</a><br>
-                    <a class="nested-paragraph" href="#chap2-sect3-para3">Para III: Du Trésorier Général/Adjoint</a><br>
-                    <a class="nested-paragraph" href="#chap2-sect3-para4">Para IV: Des Délégations Régionales</a><br>
+                    <a class="nested-paragraph" href="#chap2-sect3-para1">Para I:
+                        <?= $lang['rules']['sum-chap-2-sect-3-par1'] ?></a><br>
+                    <a class="nested-paragraph" href="#chap2-sect3-para2">Para II:
+                        <?= $lang['rules']['sum-chap-2-sect-3-par2'] ?></a><br>
+                    <a class="nested-paragraph" href="#chap2-sect3-para3">Para III:
+                        <?= $lang['rules']['sum-chap-2-sect-3-par3'] ?></a><br>
+                    <a class="nested-paragraph" href="#chap2-sect3-para4">Para IV:
+                        <?= $lang['rules']['sum-chap-2-sect-3-par4'] ?></a><br>
                 </span>
-                <a href="#chap2-sect4">Section IV : De la Commission Financière</a><br>
+                <a href="#chap2-sect4"><?= $lang['rules']['chap-2-sect-4'] ?></a><br>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingThree">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                Chapitre III : DISPOSITIONS DISCIPLINAIRES
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <?= $lang['rules']['sum-chap-3'] ?>
             </button>
         </h2>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#sommaire">
+        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap-3">Chapitre 3 : Des Dispositions Disciplinaires</a><br>
+                <a href="#chap-3"><?= $lang['rules']['sum-chap-3'] ?></a><br>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingFour">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
-                Chapitre IV : DES ACTIVITES SOCIO-CULTURELLES
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                <?= $lang['rules']['sum-chap-4'] ?>
             </button>
         </h2>
-        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#sommaire">
+        <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap-4">Chapitre 4 : Des Activités Socio-culturelles</a><br>
+                <a href="#chap-4"><?= $lang['rules']['sum-chap-4'] ?></a><br>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingFive">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                Chapitre V : DISPOSITIONS FINANCIERES
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                <?= $lang['rules']['sum-chap-5'] ?>
             </button>
         </h2>
-        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#sommaire">
+        <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap-5">Chapitre 5 : Des Dispositions Financières</a><br>
+                <a href="#chap-5"><?= $lang['rules']['sum-chap-5'] ?></a><br>
             </div>
         </div>
     </div>
     <div class="accordion-item">
         <h2 class="accordion-header" id="headingSix">
-            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
-                Chapitre VI : DES DISPOSITIONS DIVERSES ET FINALES
+            <button class="accordion-button collapsed text-uppercase" type="button" data-bs-toggle="collapse"
+                data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                <?= $lang['rules']['sum-chap-6'] ?>
             </button>
         </h2>
-        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#sommaire">
+        <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix"
+            data-bs-parent="#sommaire">
             <div class="accordion-body">
-                <a href="#chap-6">Chapitre 6 : Des Dispositions Diverses et Finales</a><br>
+                <a href="#chap-6"><?= $lang['rules']['sum-chap-6'] ?></a><br>
             </div>
         </div>
     </div>
