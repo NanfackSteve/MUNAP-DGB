75570d8773afd2cc2e4f701c222aca6ed5a695d9
Nanfack Steve <nanfacksteve7@gmail.com>
2023-11-23 15:33:48 +0100

    refactoring code

--- team_predecessor.php
+++ team_predecessor.php
@@ -1,13 +1,5 @@
 <?php session_start();
-
-// Choisis la langue
-if (isset($_SESSION['lang']) && $_SESSION['lang'] === 'fr') {
-    $lang = json_decode(file_get_contents('./language/fr.json'), true);
-} else if (isset($_SESSION['lang']) && $_SESSION['lang'] === 'en') {
-    $lang = json_decode(file_get_contents('./language/en.json'), true);
-} else {
-    $lang = json_decode(file_get_contents('./language/fr.json'), true);
-} ?>
+include_once("language/lang.php"); ?>
 
 <!DOCTYPE html>
 <html lang="fr">
