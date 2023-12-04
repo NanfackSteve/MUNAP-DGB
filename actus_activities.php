808e8435ecc3d546feb9b511c2234111af68e58a
Nanfack Steve <nanfacksteve7@gmail.com>
2023-12-04 12:10:02 +0100

    add activ-4

--- actus_activities.php
+++ actus_activities.php
@@ -4,144 +4,162 @@ include_once("language/lang.php"); ?>
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="utf-8">
-    <title><?= $lang['news']['title-page'] ?></title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <meta content="" name="keywords">
-    <meta content="" name="description">
+    <head>
+        <meta charset="utf-8">
+        <title><?= $lang['news']['title-page'] ?></title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="" name="keywords">
+        <meta content="" name="description">
 
-    <!-- Favicon -->
-    <link href="img/logo-munap.jpg" rel="icon">
+        <!-- Favicon -->
+        <link href="img/logo-munap.jpg" rel="icon">
 
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet">
+        <!-- Google Web Fonts -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap"
+            rel="stylesheet">
 
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+        <!-- Icon Font Stylesheet -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 
-    <!-- Libraries Stylesheet -->
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <!-- Libraries Stylesheet -->
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
 
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- Customized Bootstrap Stylesheet -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-</head>
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+    </head>
 
-<body>
+    <body>
 
-    <!-- Nav bar Start -->
-    <?php include_once("header.php") ?>
-    <!-- Nav bar End -->
+        <!-- Nav bar Start -->
+        <?php include_once("header.php") ?>
+        <!-- Nav bar End -->
 
-    <!-- Page Header Start -->
-    <div class="container-fluid page-header-news py-5 mb-5">
-        <div class="container py-5">
-            <h1 class="display-3 text-white mb-3 animated slideInDown"><?= $lang['news']['activ']['head-title'] ?>
-            </h1>
-        </div>
-    </div>
-    <!-- Page Header End -->
-
-
-    <!-- Projects Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="section-title text-center">
-                <h1 class="display-5 mb-5"><?= $lang['news']['activ']['section-title'] ?></h1>
-            </div>
-            <div class="row mt-n2 wow fadeInUp" data-wow-delay="0.3s">
-                <div class="col-12 text-center">
-                    <ul class="list-inline mb-5" id="portfolio-flters">
-                        <li class="mx-2 active" data-filter="*"><?= $lang['news']['filter-all'] ?></li>
-                        <li class="mx-2" data-filter=".first"><?= $lang['news']['activ']['filter-1'] ?></li>
-                        <li class="mx-2" data-filter=".second"><?= $lang['news']['activ']['filter-2'] ?></li>
-                        <li class="mx-2" data-filter=".third"><?= $lang['news']['activ']['filter-3'] ?></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="row g-4 portfolio-container">
-                <div class="col-lg-4 col-md-6 portfolio-item third wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="rounded overflow-hidden">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100 " src="img/actus/activities/actus_bus_A_3.jpg" alt="" style="height: 250px;">
-                            <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/actus/activities/actus_bus_A_3.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_2.php"><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-1'] ?></p>
-                            <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-2'] ?></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.3s">
-                    <div class="rounded overflow-hidden">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/actus/activities/actus_retraite_A_4.jpg" alt="" style="height: 250px;">
-                            <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/actus/activities/actus_retraite_A_4.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_3.php"><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-3'] ?></p>
-                            <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-4'] ?> </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.5s">
-                    <div class="rounded overflow-hidden">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/actus/activities/actus_meeting_A_0.jpg" alt="" style="height: 250px;">
-                            <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/actus/activities/actus_meeting_A_0.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_1.php"><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-5'] ?></p>
-                            <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-6'] ?></a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="rounded overflow-hidden">
-                        <div class="position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/actus/activities/actus_meeting_B_2.jpg" alt="" style="height: 250px;">
-                            <div class="portfolio-overlay">
-                                <a class="btn btn-square btn-outline-light mx-1" href="img/actus/activities/actus_meeting_B_2.jpg" data-lightbox="portfolio"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_4.php"><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="border border-5 border-light border-top-0 p-4">
-                            <p class="text-primary fw-medium mb-2">Ministère des Finances : 30 Nov. 2023</p>
-                            <h5 class="lh-base mb-0">25 ième Session Ordinaire du Conseil d'Administration</a>
-                        </div>
-                    </div>
-                </div>
+        <!-- Page Header Start -->
+        <div class="container-fluid page-header-news py-5 mb-5">
+            <div class="container py-5">
+                <h1 class="display-3 text-white mb-3 animated slideInDown"><?= $lang['news']['activ']['head-title'] ?>
+                </h1>
             </div>
         </div>
-    </div>
-    <!-- Projects End -->
+        <!-- Page Header End -->
 
 
-    <!-- Footer Start -->
-    <?php include_once("footer.php") ?>
-    <!-- Footer End -->
+        <!-- Projects Start -->
+        <div class="container-xxl py-5">
+            <div class="container">
+                <div class="section-title text-center">
+                    <h1 class="display-5 mb-5"><?= $lang['news']['activ']['section-title'] ?></h1>
+                </div>
+                <div class="row mt-n2 wow fadeInUp" data-wow-delay="0.3s">
+                    <div class="col-12 text-center">
+                        <ul class="list-inline mb-5" id="portfolio-flters">
+                            <li class="mx-2 active" data-filter="*"><?= $lang['news']['filter-all'] ?></li>
+                            <li class="mx-2" data-filter=".first"><?= $lang['news']['activ']['filter-1'] ?></li>
+                            <li class="mx-2" data-filter=".second"><?= $lang['news']['activ']['filter-2'] ?></li>
+                            <li class="mx-2" data-filter=".third"><?= $lang['news']['activ']['filter-3'] ?></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row g-4 portfolio-container">
+                    <div class="col-lg-4 col-md-6 portfolio-item third wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="rounded overflow-hidden">
+                            <div class="position-relative overflow-hidden">
+                                <img class="img-fluid w-100 " src="img/actus/activities/actus_bus_A_3.jpg" alt=""
+                                    style="height: 250px;">
+                                <div class="portfolio-overlay">
+                                    <a class="btn btn-square btn-outline-light mx-1"
+                                        href="img/actus/activities/actus_bus_A_3.jpg" data-lightbox="portfolio"><i
+                                            class="fa fa-eye"></i></a>
+                                    <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_2.php"><i
+                                            class="fa fa-link"></i></a>
+                                </div>
+                            </div>
+                            <div class="border border-5 border-light border-top-0 p-4">
+                                <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-1'] ?></p>
+                                <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-2'] ?></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.3s">
+                        <div class="rounded overflow-hidden">
+                            <div class="position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/actus/activities/actus_retraite_A_4.jpg" alt=""
+                                    style="height: 250px;">
+                                <div class="portfolio-overlay">
+                                    <a class="btn btn-square btn-outline-light mx-1"
+                                        href="img/actus/activities/actus_retraite_A_4.jpg" data-lightbox="portfolio"><i
+                                            class="fa fa-eye"></i></a>
+                                    <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_3.php"><i
+                                            class="fa fa-link"></i></a>
+                                </div>
+                            </div>
+                            <div class="border border-5 border-light border-top-0 p-4">
+                                <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-3'] ?></p>
+                                <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-4'] ?> </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.5s">
+                        <div class="rounded overflow-hidden">
+                            <div class="position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/actus/activities/actus_meeting_A_0.jpg" alt=""
+                                    style="height: 250px;">
+                                <div class="portfolio-overlay">
+                                    <a class="btn btn-square btn-outline-light mx-1"
+                                        href="img/actus/activities/actus_meeting_A_0.jpg" data-lightbox="portfolio"><i
+                                            class="fa fa-eye"></i></a>
+                                    <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_1.php"><i
+                                            class="fa fa-link"></i></a>
+                                </div>
+                            </div>
+                            <div class="border border-5 border-light border-top-0 p-4">
+                                <p class="text-primary fw-medium mb-2"><?= $lang['news']['activ']['Activities-5'] ?></p>
+                                <h5 class="lh-base mb-0"><?= $lang['news']['activ']['Activities-6'] ?></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 portfolio-item first wow fadeInUp" data-wow-delay="0.1s">
+                        <div class="rounded overflow-hidden">
+                            <div class="position-relative overflow-hidden">
+                                <img class="img-fluid w-100" src="img/actus/activities/actus_meeting_B_2.jpg" alt=""
+                                    style="height: 250px;">
+                                <div class="portfolio-overlay">
+                                    <a class="btn btn-square btn-outline-light mx-1"
+                                        href="img/actus/activities/actus_meeting_B_2.jpg" data-lightbox="portfolio"><i
+                                            class="fa fa-eye"></i></a>
+                                    <a class="btn btn-square btn-outline-light mx-1" href="actus_activ_4.php"><i
+                                            class="fa fa-link"></i></a>
+                                </div>
+                            </div>
+                            <div class="border border-5 border-light border-top-0 p-4">
+                                <p class="text-primary fw-medium mb-2">Ministère des Finances : 30 Nov. 2023</p>
+                                <h5 class="lh-base mb-0">25 ième Session Ordinaire du Conseil d'Administration</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Projects End -->
 
-    <script>
+
+        <!-- Footer Start -->
+        <?php include_once("footer.php") ?>
+        <!-- Footer End -->
+
+        <script>
         let List_header = document.getElementsByClassName('nav-link');
         for (let i = 0; i < List_header.length; i++) List_header[i].setAttribute("class", "nav-link");
         document.getElementById('actus').setAttribute("class", "nav-link active");
-    </script>
-</body>
+        </script>
+    </body>
 
 </html>
