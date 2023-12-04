f6bc245e7d32e3e3defcba0e7d7aac60632b1ad6
Nanfack Steve <nanfacksteve7@gmail.com>
2023-12-04 13:29:47 +0100

    update Profile

--- models/profile.php
+++ models/profile.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['num_adhesion'])) header("Location: login.php ");
 
 // URL de l'API cible
 $api = json_decode(file_get_contents('./models/api.json'), true);
-$url = $api['link'] . "/api/infosglesmembre/?trimestre=4&structure_id=-1&annee=2023&num_adhesion=" . $_SESSION['num_adhesion'];
+$url = $api['link'] . "/api/infosglesmembre/?trimestre=4&structure_id=-1&annee=" . date('Y') . "&num_adhesion=" . $_SESSION['num_adhesion'];
 
 // Requete + Reponse cURL
 $ch = curl_init($url);
@@ -12,27 +12,29 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $_SESSION['token'],));
 $rep = curl_exec($ch);
 $data = json_decode($rep, true);
-print_r($data);
+// print_r($data);
 curl_close($ch);
 
 // infos membres
 $num_adhesion = $_SESSION['num_adhesion'];
 $nom = $_SESSION['nom'];
 $prenom = $_SESSION['prenom'];
-//$matricule = $_SESSION['matricule'];
-//$ne_le = $data['membre_details'][0]['date_naissance'];
-$statut = $data['membre_details'][0]['status'];
-$date_adhesion = $data['membre_details'][0]['date_adhesion'];
+$structure = $data['membres'][0]['abv'];
+$fonction = $data['membres'][0]['fonction'];
+$statut = $data['membres'][0]['statut'];
+// $date_adhesion = $data['membres'][0]['date_adhesion'];
 
 // infos cotisations
-$admin = $data['membre_details'][0]['abv'];
-$fonction = $data['membre_details'][0]['libelle_fonction'];
-$region = $data['membre_details'][0]['libelle_region'];
-$qualite = $data['membre_details'][0]['libelle_qualite'];
-$frais_adhesion = $data['membre_details'][0]['frais_adhesion'];
-$cotisation = $data['membre_cotisation'];;
+$cotisation_parent = $data['membres'][0]['montantacotiserparent'];
+$cotisation_conjoint = $data['membres'][0]['montantacotiserenfant'];
+$cotisation_enfant = $data['membres'][0]['montantacotiserconjoint'];
+$cotisation_mutualiste = $data['membres'][0]['montantannuelacotiser'];
+$cotisation_total = $data['membres'][0]['montantannuelacotiserglobal'];
+$cotisation_verse = $data['membres'][0]['montantverse'];
+$cotisation_reste = $data['membres'][0]['resteacotiser'];
+
 
 // infos affiliations
-$nbr_parent = $data['nbre_parent'];
-$nbr_conjoint = $data['nbre_conjoint'];
-$nbr_enfant = $data['nbre_enfant'];
+$nbr_parent = $data['membres'][0]['nbreparent'];
+$nbr_conjoint = $data['membres'][0]['nbreconjoint'];
+$nbr_enfant = $data['membres'][0]['nbreenfant'];
