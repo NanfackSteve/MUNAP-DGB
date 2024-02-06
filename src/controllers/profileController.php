7b8a72149a29fa0d047b86fb52c87bafea27eb1c
Nanfack Steve <nanfacksteve7@gmail.com>
2024-02-06 08:24:51 +0100

    enhence profile menu (add redirection)

--- src/controllers/profileController.php
+++ src/controllers/profileController.php
@@ -7,6 +7,9 @@ class Profile
     public function execute()
     {
         session_start();
+        // If User is not connected, go to login Page !
+        if (!isset($_SESSION['num_adhesion'])) header("Location: index.php?menu=login");
+
         include_once('src/models/language/lang.php');
         include_once('src/models/profileModel.php');
         require_once('src/views/profilePage.php');
